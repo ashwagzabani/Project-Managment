cde11a41bc0bf604fb25102b9c5d27a585a24b77
ashwagzabani <ashwag.amz@gmail.com>
2021-01-31 23:37:28 +0300

    solve merge conflict

--- back_end/routes/Projects.js
+++ back_end/routes/Projects.js
@@ -32,30 +32,14 @@ router.get("/projects", (req, res) => {
   );
 });
 
-<<<<<<< HEAD
-
 router.get('/projects/:id/:role', (req, res) => {
     projects.find({
         //get user's projects
         // { "members.userId": userId },
 
         //get user's projects with role === '...'
-        members: { $elemMatch: { userId: req.params.id, role: "manager" } },
+        members: { $elemMatch: { userId: req.params.id, role: "manager" } }},
         // members: { $elemMatch: { userId: userId } }
-=======
-router.get("/projects/:id/:role", (req, res) => {
-  projects.find(
-    {
-      //get user's projects
-      // { "members.userId": userId },
-
-      //get user's projects with role === '...'
-      members: {
-        $elemMatch: { userId: req.params.role, role: req.params.role },
-      },
-      // members: { $elemMatch: { userId: userId } }
->>>>>>> 94e04cd169d26c0770ec29d2b6e113b8947f8589
-    },
     (err, result) => {
       if (err) {
         console.log(err);
@@ -66,23 +50,5 @@ router.get("/projects/:id/:role", (req, res) => {
   );
 });
 
-<<<<<<< HEAD
-
-
-//User.findOne({'local.rooms': {$elemMatch: {name: req.body.username}}}, function (err, user) {
-
-
-// router.post('/signIn', (req, res) => {
-//     users.create(req.body)
-//     users.find({}, (err, result) => {
-//         if (err) {
-//             res.json("error :(")
-//         }
-//         res.json(result);
-//     })
-// });
-
-=======
->>>>>>> 94e04cd169d26c0770ec29d2b6e113b8947f8589
 // Export the Router so we can use it in the server.js file
 module.exports = router;
--- back_end/routes/SignUp.js
+++ back_end/routes/SignUp.js
@@ -1,32 +1,9 @@
-const express = require('express');
-<<<<<<< HEAD
-=======
-const users = require('../models/Users.js')
-//you see this file and the project folder?
-
-// Instantiate a Router (mini app that only handles routes)
->>>>>>> 94e04cd169d26c0770ec29d2b6e113b8947f8589
+const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD:back_end/routes/SignUp.js
-
-users.create()
-/**
- * Action:      INDEX
- * Method:      GET
- * URI:         /
- * Description: Get the Root Route
- */
-
-//------------------------------------------
+const users = require("../models/Users");
 //SignUp request to create a user dependent in schema
-router.post('/signUp', async (req, res) => {
-  console.log('/signUp')
-=======
-const users= require('../models/Users');
-//SignUp request to create a user dependent in schema
-router.post('/signUp', async (req, res) => {
-  console.log('/signUp');
->>>>>>> a946868be1e55c81862dd1e3f2bbd393f5278d70:back_end/routes/index.js
+router.post("/signUp", async (req, res) => {
+  console.log("/signUp");
   //to add new user into DB
   const signedUpUser = new users({
     //cheack in post man ---DONE
@@ -35,25 +12,27 @@ router.post('/signUp', async (req, res) => {
     //grap the email that user enter in body req (when the user enter into submit)
     email: req.body.email,
     //grap the email that user enter in body req (when the user enter into submit)
-    password: req.body.password
+    password: req.body.password,
   });
-  let userExisit = await verifyUser(req)
+  let userExisit = await verifyUser(req);
   if (userExisit) {
     //prevent
-    res.send('Already Exist')   //to go outside function
+    res.send("Already Exist"); //to go outside function
     return;
   }
   //we need to save it
-  signedUpUser.save()
-    .then(data => {
-      res.json(data)
-    }).catch(error => {
-      res.json(error)
+  signedUpUser
+    .save()
+    .then((data) => {
+      res.json(data);
     })
-})
+    .catch((error) => {
+      res.json(error);
+    });
+});
 async function verifyUser(req) {
   try {
-    const userExisit = await users.findOne({ email: req.body.email })
+    const userExisit = await users.findOne({ email: req.body.email });
     return userExisit;
     // next();
   } catch (error) {
