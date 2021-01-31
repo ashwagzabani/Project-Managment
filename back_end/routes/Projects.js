afb201ec0da0c4699b633ad8cec0b50992131c31
30_Ashwag Win_SEI-14 <ashwag.amz@gmail.com>
2021-01-31 23:44:39 +0300

    solve merge conflict (#18)

--- back_end/routes/Projects.js
+++ back_end/routes/Projects.js
@@ -1,5 +1,4 @@
 // Require necessary NPM packages
-
 const express = require("express");
 const projects = require("../models/Projects.js");
 const Users = require("../models/Users.js");
@@ -8,7 +7,7 @@ const Users = require("../models/Users.js");
 const router = express.Router();
 
 //this user has two project: one as team member and another as manager his name: 'Najed'
-const userId = '60115b690ba0311c388c9aa8';
+const userId = "6012b20254cb2f31685b14a4";
 
 /**
  * Action:      INDEX
@@ -16,33 +15,31 @@ const userId = '60115b690ba0311c388c9aa8';
  * URI:         /projects
  * Description: Get the user's projects as manager or member
  */
+router.get("/projects", (req, res) => {
+  projects.find(
+    {
+      //get user's projects
+        //  "members.userId": userId
+        // members: { $elemMatch: { userId: userId } },
+    },
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      res.json(result);
+      console.log(result);
+    }
+  );
+});
 
-router.get('/projects/:id', (req, res) => {
+router.get('/projects/:id/:role', (req, res) => {
     projects.find({
         //get user's projects
         // { "members.userId": userId },
 
         //get user's projects with role === '...'
-        //        members: { $elemMatch: { userId: userId, role: 'manager' } }},
-        members: { $elemMatch: { userId: userId } }
-    },
-        // user.findById({ "_id": userId },
-        (err, result) => {
-            if (err) {
-                console.log(err);
-            }
-            res.json(result);
-            console.log(result)
-        });
-});
-
-router.get("/projects", (req, res) => {
-  projects.find(
-    {
-      //get user's projects
-      "members.userId": req.params.id
-      // members: { $elemMatch: { userId: userId } },
-    },
+        members: { $elemMatch: { userId: req.params.id, role: "manager" } }},
+        // members: { $elemMatch: { userId: userId } }
     (err, result) => {
       if (err) {
         console.log(err);
@@ -52,75 +49,6 @@ router.get("/projects", (req, res) => {
     }
   );
 });
-
-router.get("/projects/:id/:role", (req, res) => {
-  projects.find(
-    {
-      //get user's projects
-      // { "members.userId": userId },
-
-      //get user's projects with role === '...'
-      // members: { $elemMatch: { userId: req.params.id, role: "manager" } },      members: {
-      $elemMatch: { userId: req.params.id, role: req.params.role }
-    },
-    // members: { $elemMatch: { userId: userId } }
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      }
-      res.json(result);
-      console.log(result);
-    }
-  );
-});
-
-
-router.get("/projects/:projectId", (req, res) => {
-  projects.findOne({ "_id": req.params.projectId },
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      }
-      res.json(result);
-      console.log(result);
-    }
-  );
-});
-
-//User.findOne({'local.rooms': {$elemMatch: {name: req.body.username}}}, function (err, user) {
-
-router.get("/project/:id", (req, res) => {
-  projects.find({ "_id": req.params.id },
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      }
-      res.json(result);
-      console.log(result);
-    }
-  );
-});
-
-router.get("/testAllprojecs", (req, res) => {
-  projects.find({},
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      }
-      res.json(result);
-      console.log(result);
-    }
-  );
-});
-// router.post('/signIn', (req, res) => {
-//     users.create(req.body)
-//     users.find({}, (err, result) => {
-//         if (err) {
-//             res.json("error :(")
-//         }
-//         res.json(result);
-//     })
-// });
 
 // Export the Router so we can use it in the server.js file
 module.exports = router;
--- back_end/routes/SignUp.js
+++ back_end/routes/SignUp.js
@@ -1,27 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const users = require('../models/Users.js')
-//you see this file and the project folder?
-
-users.create()
-/**
- * Action:      INDEX
- * Method:      GET
- * URI:         /
- * Description: Get the Root Route
- */
-
-//------------------------------------------
+const users = require("../models/Users");
 //SignUp request to create a user dependent in schema
-
-//SignUp request to create a user dependent in schema
-router.post('/signUp', async (req, res) => {
-  console.log('/signUp');
-  //to add new user into DB
-  var signedUpUser = new users({
-    //cheack in post man ---DONE
-    //userName field grap the user name from the body request which  mean the user name that write it by user in input form
-    userName: req.body.userName,
+router.post("/signUp", async (req, res) => {
+  console.log("/signUp");
   //to add new user into DB
   const signedUpUser = new users({
     //cheack in post man ---DONE
@@ -30,25 +12,27 @@ router.post('/signUp', async (req, res) => {
     //grap the email that user enter in body req (when the user enter into submit)
     email: req.body.email,
     //grap the email that user enter in body req (when the user enter into submit)
-    password: req.body.password
+    password: req.body.password,
   });
-  let userExisit = await verifyUser(req)
+  let userExisit = await verifyUser(req);
   if (userExisit) {
     //prevent
-    res.send('Already Exist')   //to go outside function
+    res.send("Already Exist"); //to go outside function
     return;
   }
   //we need to save it
-  signedUpUser.save()
-    .then(data => {
-      res.json(data)
-    }).catch(error => {
-      res.json(error)
+  signedUpUser
+    .save()
+    .then((data) => {
+      res.json(data);
     })
-})
+    .catch((error) => {
+      res.json(error);
+    });
+});
 async function verifyUser(req) {
   try {
-    const userExisit = await users.findOne({ email: req.body.email })
+    const userExisit = await users.findOne({ email: req.body.email });
     return userExisit;
     // next();
   } catch (error) {
