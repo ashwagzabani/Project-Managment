e2bc196e982de234a704ae0d5e452f07ea958159
ashwagzabani <ashwag.amz@gmail.com>
2021-02-03 23:37:07 +0300

    final deployment

--- back_end/routes/project/Projects.js
+++ back_end/routes/project/Projects.js
@@ -8,30 +8,29 @@ const Users = require("../../models/Users.js");
 const router = express.Router();
 
 //this user has two project: one as team member and another as manager his name: 'Najed'
-const userId = '60115b690ba0311c388c9aa8';
 /**
  * Action:      INDEX
  * Method:      GET
  * URI:         /projects
  * Description: Get the user's projects as manager or member
  */
-router.get('/projects/:id', (req, res) => {
-  projects.find({
-    //get user's projects
-    // { "members.userId": userId },
-
-    //get user's projects with role === '...'
-    //        members: { $elemMatch: { userId: userId, role: 'manager' } }},
-    members: { $elemMatch: { userId: req.params.id } }
-  },
+router.get("/projects/:id", (req, res) => {
+  projects.find(
+    {
+      //get user's projects
+      // { "members.userId": userId },
+      //get user's projects with role === '...'
+      members: { $elemMatch: { userId: req.params.id } },
+    },
     // user.findById({ "_id": userId },
     (err, result) => {
       if (err) {
         console.log(err);
       }
       res.json(result);
-      console.log(result)
-    });
+      console.log(result);
+    }
+  );
 });
 // router.get("/projects", (req, res) => {
 router.get("/project/:id", (req, res) => {
@@ -54,7 +53,6 @@ router.get("/projects", (req, res) => {
   projects.find(
     {
       //get user's projects
-
     },
     (err, result) => {
       if (err) {
@@ -65,37 +63,29 @@ router.get("/projects", (req, res) => {
     }
   );
 });
-// });
-router.get('/projects/:id/:role', (req, res) => {
-  projects.find({
-    //get user's projects
-    // { "members.userId": userId },
 
-    //get user's projects with role === '...'
-    members: { $elemMatch: { userId: req.params.role, role: req.params.role } },
-    // members: { $elemMatch: { userId: userId } }
-  },
+router.get("/projects/:id/:role", (req, res) => {
+  projects.find(
+    {
+      //get user's projects
+      // { "members.userId": userId },
+
+      //get user's projects with role === '...'
+      members: {
+        $elemMatch: { userId: req.params.role, role: req.params.role },
+      },
+      // members: { $elemMatch: { userId: userId } }
+    },
     // user.findById({ "_id": userId },
     (err, result) => {
       if (err) {
         console.log(err);
       }
       res.json(result);
-      console.log(result)
-    });
+      console.log(result);
+    }
+  );
 });
-
-//User.findOne({'local.rooms': {$elemMatch: {name: req.body.username}}}, function (err, user) {
-
-// router.post('/signIn', (req, res) => {
-//     users.create(req.body)
-//     users.find({}, (err, result) => {
-//         if (err) {
-//             res.json("error :(")
-//         }
-//         res.json(result);
-//     })
-// });
 
 // Export the Router so we can use it in the server.js file
 module.exports = router;
