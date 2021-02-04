2db4f0fdb74bfba43d8dba6cccb5ff592752d05c
ashwagzabani <ashwag.amz@gmail.com>
2021-02-04 08:36:26 +0300

    deploy

--- back_end/routes/project/UpdateProject.js
+++ back_end/routes/project/UpdateProject.js
@@ -11,34 +11,46 @@ const router = express.Router();
  * URI:         /project/id/
  * Description: Update an esisting project status, Date and/or title
  */
-router.patch('/project/update/:id', (req, res) => {
+router.patch('/project/:id', (req, res) => {
 
-  projects.findByIdAndUpdate({ _id: req.params.id }, req.body, (error, result) => {
+  projects.findByIdAndUpdate({ "_id": req.params.id }, req.body, (error, result) => {
     if (error) {
       console.log(error);
       res.json({ error: "(" });
     }
-    else {
-      projects.findById(
-        {
-          //get user's projects
-          _id: req.params.id,
-        },
-        (err, result) => {
-          if (err) {
-            console.log(err);
-          } else {
-            res.json(result);
-            console.log("this from back end:", result);
-          }
-
-        }
-      );
-    }
-    // console.log(req.body);
-    // res.json(result); 
+    console.log(req.body);
+    res.json(result);
   })
 });
+
+// router.patch('/project/update/:id', (req, res) => {
+
+//   // projects.findByIdAndUpdate({ _id: req.params.id }, req.body, (error, result) => {
+//   //   if (error) {
+//   //     console.log(error);
+//   //     res.json({ error: "(" });
+//   //   }
+//   //   else {
+//   //     projects.findById(
+//   //       {
+//   //         //get user's projects
+//   //         _id: req.params.id,
+//   //       },
+//   //       (err, result) => {
+//   //         if (err) {
+//   //           console.log(err);
+//   //         } else {
+//   //           res.json(result);
+//   //           console.log("this from back end:", result);
+//   //         }
+
+//   //       }
+//   //     );
+//   //   }
+//   //   // console.log(req.body);
+//   //   // res.json(result); 
+//   // })
+// });
 
 // Export the Router so we can use it in the server.js file
 module.exports = router;
